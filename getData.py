97147fe1a6014a000a5ae1e411fc3147338008a0
jvs-alpha <jvsamuel.rufus@gmail.com>
2020-02-15 00:10:43 +0530

    added changes to the getData

--- getData.py
+++ getData.py
@@ -2,12 +2,21 @@ from urllib import request
 import json
 import datetime
 
-site = "https://poloniex.com/public?command=returnChartData&currencyPair=USDC_BTC&start=1420070400&end=15816986170&period=14400"
+site = "https://poloniex.com/public?command=returnChartData&currencyPair=USDC_BTC&start=1420070400&end=15816986170&period=14400&depth=10"
 rawdata = request.urlopen(site)
 jsondata = json.load(rawdata)
 check = 0
 for i in jsondata:
-    check += 1
+    if check == 10:
+        break
     date = datetime.datetime.fromtimestamp(i["date"])
-    print(date)
-print(check)
+    high = i["high"]
+    low = i["low"]
+    open = i["open"]
+    close = i["close"]
+    volume = i["volume"]
+    quotevolume = i["quoteVolume"]
+    weightedaverage = i["weightedAverage"]
+    print("Date: {}\nHigh: {}\nLow: {}\nOpen: {}\nClose: {}\nVolume: {}\nQuoteVolume: {}\nWeightedAverage: {}".format(date,high,low,open,close,volume,quotevolume,weightedaverage))
+    print()
+    check += 1
