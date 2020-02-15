7944310ee920c1acca7f91d02df0ca484f43505d
jvs-alpha <jvsamuel.rufus@gmail.com>
2020-02-15 21:11:03 +0530

    made tha data set

--- getData.py
+++ getData.py
@@ -1,22 +1,25 @@
 from urllib import request
 import json
 import datetime
+import csv
 
 site = "https://poloniex.com/public?command=returnChartData&currencyPair=USDC_BTC&start=1420070400&end=15816986170&period=14400&depth=10"
 rawdata = request.urlopen(site)
 jsondata = json.load(rawdata)
-check = 0
-for i in jsondata:
-    if check == 10:
-        break
-    date = datetime.datetime.fromtimestamp(i["date"])
-    high = i["high"]
-    low = i["low"]
-    open = i["open"]
-    close = i["close"]
-    volume = i["volume"]
-    quotevolume = i["quoteVolume"]
-    weightedaverage = i["weightedAverage"]
-    print("Date: {}\nHigh: {}\nLow: {}\nOpen: {}\nClose: {}\nVolume: {}\nQuoteVolume: {}\nWeightedAverage: {}".format(date,high,low,open,close,volume,quotevolume,weightedaverage))
-    print()
-    check += 1
+with open("data.csv","w") as csvfile:
+    fieldnames = ["date","high","low","open","close","volume","quoteVolume","weightedAverage"]
+    writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+    writer.writeheader()
+    
+    for i in jsondata:
+        date = datetime.datetime.fromtimestamp(i["date"])
+        high = i["high"]
+        low = i["low"]
+        open = i["open"]
+        close = i["close"]
+        volume = i["volume"]
+        quoteVolume = i["quoteVolume"]
+        weightedAverage = i["weightedAverage"]
+        writer.writerow({"date":date,"high":high,"low":low,"open":open,"close":close,"volume":volume,"quoteVolume":quoteVolume,"weightedAverage":weightedAverage})
+        print("Date: {}\nHigh: {}\nLow: {}\nOpen: {}\nClose: {}\nVolume: {}\nQuoteVolume: {}\nWeightedAverage: {}".format(date,high,low,open,close,volume,quoteVolume,weightedAverage))
+        print()
