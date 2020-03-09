03600fc7fc7a848f752835dc38e3b2bf4eb8f170
jvs-alpha <jvsamuel.rufus@gmail.com>
2020-03-09 13:29:08 +0530

    added BitcoinML2

---
+++ BitcoinML2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from keys import *
+from binance.client import Client
+from sklearn.preprocessing import MinMaxScaler
+
+
+# dataset = pd.read_csv("data.csv")
+
+client = Client(APIKey,SecretKey)
+symbol = "BTCUSDT"
+BTC = client.get_historical_klines(symbol=symbol,interval=Client.KLINE_INTERVAL_30MINUTE,start_str="1 year ago UTC")
+BTC = pd.DataFrame(BTC,columns=["Open time","Open","High","Low","Close","Volume","Close time","Quote asset volume","Number of trades","Taker buy base asset volume","Taker buy quote asset volume","Ignore"])
+BTC["Open time"] = pd.to_datetime(BTC["Open time"],unit="ms")
+BTC.set_index("Open time",inplace=True)
+BTC["Close"] = BTC["Close"].astype("float")
+data = BTC.iloc[:,[3]].astype("float").values
+scaler = MinMaxScaler()
+data = scaler.fit_transform(data)
+print(data)
--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Bitcoin-NeuralNetwork
 
 ## TODO
-* __Finish the Example__
-* __Find the meaning of the values in the example program__
 * Understand how to create a neuralnetwork for analysing values
 * Make the system modular so it can be improved in the future
 * Make the project properly so it is actually useful
