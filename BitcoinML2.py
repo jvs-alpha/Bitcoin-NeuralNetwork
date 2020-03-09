92b75e91900f5c81ea8a0df3bf7ac1e8ee3ed162
jvs-alpha <jvsamuel.rufus@gmail.com>
2020-03-09 19:33:25 +0530

    added chnages to the BitcoinML2

--- BitcoinML2.py
+++ BitcoinML2.py
@@ -2,19 +2,25 @@ import pandas as pd
 from keys import *
 from binance.client import Client
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from keras.models import Sequential
+from keras.layers import LSTM,Dense
 
 
-# dataset = pd.read_csv("data.csv")
 
-client = Client(APIKey,SecretKey)
-symbol = "BTCUSDT"
-BTC = client.get_historical_klines(symbol=symbol,interval=Client.KLINE_INTERVAL_30MINUTE,start_str="1 year ago UTC")
-BTC = pd.DataFrame(BTC,columns=["Open time","Open","High","Low","Close","Volume","Close time","Quote asset volume","Number of trades","Taker buy base asset volume","Taker buy quote asset volume","Ignore"])
-BTC["Open time"] = pd.to_datetime(BTC["Open time"],unit="ms")
-BTC.set_index("Open time",inplace=True)
-BTC["Close"] = BTC["Close"].astype("float")
-data = BTC.iloc[:,[3]].astype("float").values
+BTC = pd.read_csv("data.csv",index_col=0)
+BTC["close"] = BTC["close"].astype("float")
+data = BTC.iloc[:,[4]].astype("float").values
 scaler = MinMaxScaler()
 data = scaler.fit_transform(data)
-training_set = data[0:len(data)-1]
-print(len(training_set))
+
+training_set = data[:2268]
+test_set = data[2268:]
+
+x_train = training_set[0:len(training_set)-1]
+y_train = training_set[1:len(training_set)]
+
+x_test = test_set[0:len(test_set)-1]
+y_test = test_set[1:len(test_set)]
+
+print(x_train.shape)
